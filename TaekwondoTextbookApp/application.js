05fae5d1013b8fd0e859c5f5bc6e567570adee52
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-12-22 18:00:43 +0000

    Established the basic concept of the textbook app

---
+++ TaekwondoTextbookApp/application.js
@@ -0,0 +1,15 @@
+﻿var application = angular.module("TaekwondoTextbookApp", []);
+
+application.controller("TextbookController", ["$scope", function ($scope) {
+
+    $scope.page = 1;
+
+    $scope.previousPage = function () {
+        $scope.page -= 1;
+    }
+
+    $scope.nextPage = function () {
+        $scope.page += 1;
+    }
+
+}]);
