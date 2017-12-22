86804b7e579cb49bad895bbca1e7fda513f5fe26
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-12-22 19:39:19 +0000

    Began adding the structure for an exercise

--- TaekwondoTextbookApp/application.js
+++ TaekwondoTextbookApp/application.js
@@ -1,9 +1,19 @@
 ﻿var application = angular.module("TaekwondoTextbookApp", []);
 
+var exercise1Data = {
+    type: "vocabulary multiple choice",
+    questions: [
+        { term: "front", correctAnswer: "ap", incorrectAnswers: ["yeop", "dwit", "an"] },
+        { term: "rear", correctAnswer: "dwit", incorrectAnswers: ["ap", "yeop", "an"] },
+        { term: "side", correctAnswer: "yeop", incorrectAnswers: ["ap", "an", "mit"] }]
+};
+
 application.controller("TextbookController", ["$scope", function ($scope) {
 
     $scope.numberOfPages = 3;
     $scope.page = 1;
+
+    $scope.exercise1Data = exercise1Data;
 
     $scope.previousPage = function () {
         if ($scope.page <= 1) {
