ae218d0852d489980b7b9c9df5f7aa938fba811a
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-12-22 18:56:43 +0000

    Added in keyboard controls

--- TaekwondoTextbookApp/application.js
+++ TaekwondoTextbookApp/application.js
@@ -1,16 +1,30 @@
 ﻿var application = angular.module("TaekwondoTextbookApp", []);
 
 application.controller("TextbookController", ["$scope", function ($scope) {
-    
+
     $scope.numberOfPages = 2;
     $scope.page = 1;
 
     $scope.previousPage = function () {
+        if ($scope.page <= 1) {
+            return;
+        }
         $scope.page -= 1;
     }
 
     $scope.nextPage = function () {
+        if ($scope.page >= $scope.numberOfPages) {
+            return;
+        }
         $scope.page += 1;
+    }
+
+    $scope.keyPress = function (event) {
+        if (event.which == 13) {
+            $scope.nextPage();
+
+            event.preventDefault();
+        }
     }
 
 }]);
