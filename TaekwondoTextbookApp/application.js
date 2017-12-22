e017f2b2c15693f66308bb42c32c41d97ea82ce7
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-12-22 18:23:28 +0000

    Added styling

--- TaekwondoTextbookApp/application.js
+++ TaekwondoTextbookApp/application.js
@@ -1,7 +1,8 @@
 ﻿var application = angular.module("TaekwondoTextbookApp", []);
 
 application.controller("TextbookController", ["$scope", function ($scope) {
-
+    
+    $scope.numberOfPages = 2;
     $scope.page = 1;
 
     $scope.previousPage = function () {
